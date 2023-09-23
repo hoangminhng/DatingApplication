3a8f0f1efb1d173a3dc34c383cffeaf59af8bf87
HoangMinhNg <ngminh3004@gmail.com>
2023-09-23 09:51:04 +0700

    add UserController API

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // api/user
+public class UserController : ControllerBase
+{
+    private readonly DataContext context;
+    public UserController(DataContext context)
+    {
+        this.context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    {
+        var users = context.Users.ToList();
+        return users;
+    }
+
+    [HttpGet("{id}")] //api/user/2
+    public ActionResult<AppUser> GetUser(int id)
+    {
+        return context.Users.Find(id);
+    }
+
+}
